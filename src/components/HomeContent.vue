4a95c071404ae3b2381a5319984d2836db079496
schollar <cschollar22@gmail.com>
2021-12-09 18:01:38 -0700

    Changed Pagequote into its own component, page now stores quote clicked on in cookie as the whole object not just the quote

--- src/components/HomeContent.vue
+++ src/components/HomeContent.vue
@@ -1,40 +1,32 @@
 b<template>
   <div>
     <button-one @button_clicked="display_quotes"></button-one>
-    <section
-      @click="store_quote"
+    <page-quote
       v-for="quote in quotes"
       :key="quote._id"
-      :source="quote.source"
-    >
-      <h1>{{ quote.source }}</h1>
-      <h1>{{ quote.quote }}</h1>
-    </section>
+      :quote="quote"
+    ></page-quote>
   </div>
 </template>
 
 <script>
 import ButtonOne from "./ButtonOne.vue";
-import cookies from "vue-cookies";
+import PageQuote from "./PageQuote.vue";
 export default {
   name: "home-content",
   methods: {
     display_quotes(payload) {
       this.quotes = payload;
-      return this.quotes;
-    },
-    store_quote(quote) {
-      console.log(quote);
-      cookies.set("quote", quote.srcElement.innerText);
     },
   },
   data() {
     return {
-      quotes: {},
+      quotes: [],
     };
   },
   components: {
     ButtonOne,
+    PageQuote,
   },
 };
 </script>
---
+++ src/components/PageQuote.vue
@@ -0,0 +1,25 @@
+<template>
+  <div @click="store_quote" class="quote_container">
+    <p>{{ quote.quote }}</p>
+    <h4>{{ quote.source }}</h4>
+    <h6>{{ quote.philosophy }}</h6>
+  </div>
+</template>
+
+<script>
+import cookies from "vue-cookies";
+export default {
+  name: "page-quote",
+  methods: {
+    store_quote() {
+      cookies.set("quote", this.quote);
+    },
+  },
+  props: {
+    quote: Object,
+  },
+};
+</script>
+
+<style scoped>
+</style>
