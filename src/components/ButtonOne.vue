6758fc16e9aff99830b7c4fe10365f52ddea55e5
schollar <cschollar22@gmail.com>
2021-12-09 17:38:17 -0700

    Added quotes onto page, click on one and it saves it as cookie

---
+++ src/components/ButtonOne.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="get_quotes">Click Me</button>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "button-one",
+  methods: {
+    get_quotes() {
+      axios
+        .request({
+          url: "http://philosophy-quotes-api.glitch.me/quotes",
+        })
+        .then((response) => {
+          this.$emit("button_clicked", response.data);
+        })
+        .catch((error) => {
+          error;
+          this.$emit(
+            "button_clicked",
+            "Sorry, there was an error with the API"
+          );
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/HomeContent.vue
@@ -0,0 +1,43 @@
+b<template>
+  <div>
+    <button-one @button_clicked="display_quotes"></button-one>
+    <section
+      @click="store_quote"
+      v-for="quote in quotes"
+      :key="quote._id"
+      :source="quote.source"
+    >
+      <h1>{{ quote.source }}</h1>
+      <h1>{{ quote.quote }}</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+import ButtonOne from "./ButtonOne.vue";
+import cookies from "vue-cookies";
+export default {
+  name: "home-content",
+  methods: {
+    display_quotes(payload) {
+      this.quotes = payload;
+      return this.quotes;
+    },
+    store_quote(quote) {
+      console.log(quote);
+      cookies.set("quote", quote.srcElement.innerText);
+    },
+  },
+  data() {
+    return {
+      quotes: {},
+    };
+  },
+  components: {
+    ButtonOne,
+  },
+};
+</script>
+
+<style scoped>
+</style>
